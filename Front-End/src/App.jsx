c0f23098b247469edbcd1d550540748036a7105c
Ruthvik-09 <ruthvikv9@gmail.com>
2025-11-04 20:43:24 +0530

    added routing to the pakage

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,4 +1,4 @@
-
+import {Routes , Route} from 'react-router'
 import './App.css'
 import { HomePage } from './pages/HomePage'
 
@@ -6,7 +6,11 @@ function App() {
 
   return (
     <>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="checkout" element={<div>checkout page</div>} />
+      </Routes>
+      
     </>
   )
 }
